173a3649d029e6b97a4f607b09c3e5ad3c3e4cb6
Fids2323 <workfids23@gmail.com>
2022-07-16 14:57:19 +0300

    not CRA

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import fids from './img/clock 1.jpg'
+
+const Module = () => (
+	<>
+		<h1>Hello React</h1>
+		<img width={200} src={fids} />
+	</>
+);
+
+export default Module;
